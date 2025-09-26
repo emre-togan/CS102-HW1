b392513b254a1f8fa05706c0baf505c9d9b67d63
Emirhan Efe Suçeken <efe.suceken@ug.bilkent.edu.tr>
2025-09-26 16:17:11 +0300

    created array
    part a is done

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayOperations{
     public static void main(String[] args) {
+
+        System.out.print("Welcome! Please give the array size that you want: ");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int arrSize = scanner.nextInt();
+        int[] array = new int[arrSize];
+        
+        for(int i = 0; i < arrSize ; i++){
+            int randomNum = random.nextInt(101);
+            array[i] = randomNum;
+        }
         
     }
 
