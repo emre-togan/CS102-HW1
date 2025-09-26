599c12d51b1eef364437ec4b2ffae8def9c974cb
Emre Toğan <emre.togan@ug.bilkent.edu.tr>
2025-09-26 15:52:10 +0300

    Outline
    Made an outline for methods.

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,1 +1,25 @@
+public class ArrayOperations{
+    public static void main(String[] args) {
+        
+    }
 
+    public static int[] arrayMin(){
+
+    }
+
+    public static int[] arrayMax(){
+
+    }
+
+    public static int[] diffFromAve(){
+
+    }
+
+    public static int sumOfOdds( int[] numbers){
+
+    }
+
+    public static int sumOfEvens( int[] numbers){
+
+    }
+}
